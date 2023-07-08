bab4d2ac97ad5d7022d0f8b9780909eca1798bf3
Pooja Prabhu <pooja.prabhu28@gmail.com>
2023-07-08 10:36:01 +0530

    custom user profile model manager and migrations

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# configure custom user model in django
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
+
